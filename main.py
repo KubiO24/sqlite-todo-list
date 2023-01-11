674c352e0e31f4b081e549b6984792995d0a71f0
KubiO24 <kubakowal2004@gmail.com>
2023-01-11 10:00:26 +0100

    New task text and date edit

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import sys
 
-from PySide6.QtCore import QSize
+from PySide6.QtCore import QSize, Qt, QDateTime
 from PySide6.QtWidgets import (
     QApplication, QLabel, QMainWindow,
-    QPushButton, QVBoxLayout, QWidget
+    QPushButton, QVBoxLayout, QWidget, QLineEdit, QHBoxLayout, QDateEdit
 )
 
 
 class NewTaskWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setFixedSize(QSize(220, 150))
+        self.setWindowTitle("New Task")
+
         layout = QVBoxLayout()
-        self.label = QLabel("New Task Window")
-        layout.addWidget(self.label)
+
+        text_layout = QHBoxLayout()
+        text_label = QLabel("text: ")
+        text_label.setFixedWidth(40)
+        text_layout.addWidget(text_label)
+        self.text_edit = QLineEdit()
+        self.text_edit.setFixedWidth(150)
+        text_layout.addWidget(self.text_edit)
+        layout.addLayout(text_layout)
+
+        date_layout = QHBoxLayout()
+        date_label = QLabel("date: ")
+        date_label.setFixedWidth(40)
+        date_layout.addWidget(date_label)
+        self.date_edit = QDateEdit(calendarPopup=True)
+        self.date_edit.setDateTime(QDateTime.currentDateTime())
+        self.date_edit.setFixedWidth(150)
+        date_layout.addWidget(self.date_edit)
+        layout.addLayout(date_layout)
+
+        self.add_button = QPushButton("Add")
+        self.add_button.clicked.connect(self.add_task)
+        layout.addWidget(self.add_button)
+
         self.setLayout(layout)
+
+    def add_task(self):
+        text = self.text_edit.text().strip()
+        date = self.date_edit.date()
+
+        if text == "":
+            return
+
+        print(text)
+        print(date)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(QSize(200, 50))
         self.setWindowTitle("To Do List")
         self.newTaskWindow = NewTaskWindow()
 
@@ -41,7 +77,8 @@ class MainWindow(QMainWindow):
             self.newTaskWindow.show()
 
 
-app = QApplication(sys.argv)
-mainWindow = MainWindow()
-mainWindow.show()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
